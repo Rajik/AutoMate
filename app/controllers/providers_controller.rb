f49359f6369202b94451f22eafff07c075ebb4c3
Rajik <krajeswaribtech@gmail.com>
2013-12-01 13:09:58 +0530

    [Raji] added auth

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -8,11 +8,21 @@ class ProvidersController < ApplicationController
 
   def curl
     @@api_version = params[:api_version]
+    auth = params[:auth]
+
     if params[:verb].eql? 'GET'
-      @@response = Typhoeus.get(params[:url])
+      if auth.empty?
+        @@response = Typhoeus.get(params[:url])
+      else
+        @@response = Typhoeus.get(params[:url], userpwd: auth)
+      end
     else
       if params[:verb].eql? 'POST'
-        @@response = Typhoeus.post(params[:url], headers:{ "Content-Type" => "application/x-www-form-urlencoded"} , body: params[:post_body])
+        if auth.empty?
+          @@response = Typhoeus.post(params[:url], headers: {"Content-Type" => "application/x-www-form-urlencoded"}, body: params[:post_body])
+        else
+          @@response = Typhoeus.post(params[:url], headers: {"Content-Type" => "application/x-www-form-urlencoded"}, body: params[:post_body], userpwd: auth)
+        end
       end
     end
     puts @@response.response_body.as_json
@@ -23,7 +33,6 @@ class ProvidersController < ApplicationController
   end
 
   def validate
-    puts
     @errors = validate_response @@response.response_body, @@api_version.to_i
 
     puts @errors
